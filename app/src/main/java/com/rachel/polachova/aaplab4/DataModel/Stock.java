e3ffc0f710ca209c3145c0bdbe628d1748b15c0f
xpolach4 <rachel.polachova@gmail.com>
2019-04-15 13:41:50 +0300

    stock monitor

---
+++ app/src/main/java/com/rachel/polachova/aaplab4/DataModel/Stock.java
@@ -0,0 +1,27 @@
+package com.rachel.polachova.aaplab4.DataModel;
+
+public class Stocks {
+	private String name;
+	private String price;
+
+	public Stocks(String name, String price) {
+		this.name = name;
+		this.price = price;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPrice() {
+		return price;
+	}
+
+	public void setPrice(String price) {
+		this.price = price;
+	}
+}
---
+++ app/src/main/java/com/rachel/polachova/aaplab4/StockMonitorV2Activity.java
@@ -0,0 +1,13 @@
+package com.rachel.polachova.aaplab4;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StockMonitorV2Activity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_stock_monitor_v2);
+	}
+}
